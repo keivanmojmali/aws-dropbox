516c0657f13f02ef30829d09756652eb0368c643
Keivan Mojmali <mojmalik@gmail.com>
2021-10-07 19:32:40 -0400

    w

---
+++ src/routes/v1/image.upload.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDefinition = require('../../docs/swaggerDef');
+
+const router = express.Router();
+
+router.get('/',(req,res,next) => {
+  res.status(200).json({'response': 'Hello! Success'});
+})
+
+module.exports = router;
--- src/routes/v1/query.files.js
+++ src/routes/v1/query.files.js
@@ -5,20 +5,7 @@ const swaggerDefinition = require('../../docs/swaggerDef');
 
 const router = express.Router();
 
-// const specs = swaggerJsdoc({
-//   swaggerDefinition,
-//   apis: ['src/docs/*.yml', 'src/routes/v1/*.js'],
-// });
-
-router.use('/', swaggerUi.serve);
-// router.get(
-//   '/',
-//   swaggerUi.setup(specs, {
-//     explorer: true,
-//   })
-// );
-
-router.get('/test1',(req,res,next) => {
+router.get('/all',(req,res,next) => {
   res.status(200).json({'response': 'Hello! Success'});
 })
 
