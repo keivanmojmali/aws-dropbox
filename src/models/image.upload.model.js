4d5b28d2c6d2b04f7546ee4c1374ef5b598b8305
Keivan Mojmali <mojmalik@gmail.com>
2021-10-07 18:38:03 -0400

    w

--- src/models/image.upload.model.js
+++ src/models/image.upload.model.js
@@ -3,30 +3,7 @@ const { toJSON } = require('./plugins');
 
 const imageUploadSchema = mongoose.Schema(
   {
-    token: {
-      type: String,
-      required: true,
-      index: true,
-    },
-    user: {
-      type: mongoose.SchemaTypes.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    type: {
-      type: String,
-      enum: [tokenTypes.REFRESH, tokenTypes.RESET_PASSWORD, tokenTypes.VERIFY_EMAIL],
-      required: true,
-    },
-    expires: {
-      type: Date,
-      required: true,
-    },
-    blacklisted: {
-      type: Boolean,
-      default: false,
-    },
-  },
+    
   {
     timestamps: true,
   }
