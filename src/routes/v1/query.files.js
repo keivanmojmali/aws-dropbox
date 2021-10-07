f46a97bf7e8a9e598b34f5167ee669946c7c1842
Keivan Mojmali <mojmalik@gmail.com>
2021-10-07 19:48:37 -0400

    w'

--- src/routes/v1/query.files.js
+++ src/routes/v1/query.files.js
@@ -2,11 +2,20 @@ const express = require('express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDefinition = require('../../docs/swaggerDef');
+const faker = require('faker');
 
 const router = express.Router();
 
 router.get('/all',(req,res,next) => {
   res.status(200).json({'response': 'Hello! Success'});
 })
+
+router.get('/test',(req,res,next)=>{
+    let imagesArray = [];
+    for(let i =0; i < 11; i++) {
+
+    }
+})
+
 
 module.exports = router;
