c1dcde4afd5e162b2c14ac8a64713bca1f13158c
Keivan Mojmali <mojmalik@gmail.com>
2021-10-08 21:44:45 -0400

    figuring out how to upload images - then how to write a test for it

--- tests/unit/routes/v1/query.files.js
+++ tests/unit/routes/v1/query.files.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const ApiError = require('../../../src/utils/ApiError');
 const logger = require('../../../src/config/logger');
+const File = require('../../../../src/models/file.model');
 
-describe('queries', () => {
 
-})
+
